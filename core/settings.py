b8198caa0e25e0adf50ba7dab5d12c9ee08064f4
Raphael <raphaelaraujotoc@outlook.com>
2023-11-08 18:40:04 -0300

    Instalando o app autenticacao

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # apps:
     "tarefas",
+    "autenticacao",
     # 3rd apps:
     'widget_tweaks',
 ]
--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("tarefas.urls")),
+    path("", include("autenticacao.urls")),
 ]
