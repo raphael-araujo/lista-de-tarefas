e4144eb826a06ff54aee2db851c5aa2f15cf3000
Raphael <raphaelaraujotoc@outlook.com>
2023-11-09 22:00:45 -0300

    Adicionando página de cadastro

---
+++ autenticacao/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+class CadastroForm(UserCreationForm):
+    username = forms.CharField(label="username", min_length=5, max_length=150)
+    email = forms.EmailField(label="email")
+    password1 = forms.CharField(label="password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data["username"].lower()
+        novo_usuario = User.objects.filter(username=username)
+        if novo_usuario.count():
+            raise ValidationError("Este nome de usuário já existe")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data["email"].lower()
+        novo_email = User.objects.filter(email=email)
+        if novo_email.count():
+            raise ValidationError("Este e-mail já está cadastrado.")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data["password1"]
+        password2 = self.cleaned_data["password2"]
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("As senhas não coincidem")
+        return password2
+
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data["username"],
+            self.cleaned_data["email"],
+            self.cleaned_data["password1"],
+        )
+        return user
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+
+from .forms import CadastroForm
 
 # Create your views here.
+
+
+def cadastro(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = CadastroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Usuário criado com sucesso.")
+            return redirect(to="cadastro")
+    else:
+        if request.user.is_authenticated:
+            return redirect(to="index")
+        form = CadastroForm()
+
+    return render(request, "cadastro.html", {"form": form})
