0229e8fa766e28e4907244d526b13bfc59405c39
Raphael <raphaelaraujotoc@outlook.com>
2023-11-10 20:39:51 -0300

    Adicionando url de logout

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("cadastro/", views.cadastro, name="cadastro"),
     path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
 ]
